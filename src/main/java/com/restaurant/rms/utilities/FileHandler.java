0bfbfebb5233dd390e224ee2035231c908cef247
Ammaar <98556202+ammaar0x01@users.noreply.github.com>
2025-07-03 15:11:29 +0200

    Updated File Handler. Esethu Gift Mehlo

--- src/main/java/com/restaurant/rms/utilities/FileHandler.java
+++ src/main/java/com/restaurant/rms/utilities/FileHandler.java
@@ -1,5 +1,8 @@
 package com.restaurant.rms.utilities;
-
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 /**
  * Used to create the receipt
@@ -7,15 +10,39 @@ package com.restaurant.rms.utilities;
  */
 public class FileHandler {
 
-    public static boolean openFile(){
+    public static boolean openFile(File file){
+        if (file == null || !file.exists()){
+            System.out.println("file not found");
+        }
+        else {
+           try {
+               BufferedReader reader = new BufferedReader(new FileReader(file));
+               System.out.println("Opened and read file!");
+               return true;
+           } catch (Exception e) {
+               System.out.println("Error opening file");
+           }
+        }
+        return false;
+
+    }
+
+    public static boolean writeToFile(String message, File file){
+        if (openFile(file)){
+            try {
+                FileWriter writer = new FileWriter(file);
+                writer.write(message);
+                System.out.println(" Successfully wrote to the file!");
+                return true;
+            } catch (IOException e) {
+                System.out.println("Error writing to the file!");
+                e.printStackTrace();
+            }
+        }
         return false;
     }
 
-    public static boolean writeToFile(){
-        return false;
-    }
-
-    public static boolean closeFile(){
+    public static boolean closeFile(File file){
         return false;
     }
 }
